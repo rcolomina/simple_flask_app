9c33120466344487997b43c7fca7f44b38e8e276
rcolomina <ruben.colomina@telespazio.com>
2018-11-29 17:51:40 +0000

    Modified to post multiple emails on a json list.

--- test_curl_post_username_emails.sh
+++ test_curl_post_username_emails.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 username=$1
-email=$2
-first=$3
-surname=$4
-mydata="{\"username\":\"${username}\",\"email\":\"${email}\",\"firstname\":\"${first}\",\"surname\":\"${surname}\"}"
-echo "Data to send using curl:"$mydata
+
+mydata="{\"username\":\"$username\",\"email\":[\"frodobolson@mordor.com\",\"fbolson@earth.com\"],\"firstname\":\"Frodo\",\"surname\":\"BolsonCerrado\"}"
+echo "Data to send using curl: "$mydata
+
 curl --header "Content-type: application/json" --request POST --data $mydata 127.0.0.1:5000/contact/
 
